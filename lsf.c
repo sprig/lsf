cb8d92df52bcae35db1e41e574375801e123d881
Konstantin Kliakhandler <kosta@slumpy.org>
2014-07-28 19:46:52 +0300

    Changed so as to display whole path in output.

--- lsf.c
+++ lsf.c
@@ -2,6 +2,7 @@
  * See LICENSE file for license details.
  */
 
+#include <limits.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +14,10 @@ int listdir(const char*, const char*);
 
 int
 main(int argc, char *argv[]) {
-  char* dirname;
+  size_t size = 10000;
+  //  char* dirname = NULL;
+  char dirname[10000];
+
   if(argc == 1)
     exit(EXIT_FAILURE);
 
@@ -21,8 +25,9 @@ main(int argc, char *argv[]) {
     fputs("lsx-"VERSION", (C)opyright 2014 Konstantin Kliakhandler\n", stdout);
     exit(EXIT_SUCCESS);
   }
-  dirname = get_current_dir_name();
-  listdir(dirname, argv[1]);
+
+  if (getcwd(dirname,size) != NULL)
+    listdir(dirname, argv[1]);
   return 0;
 };
 
@@ -30,22 +35,24 @@ int listdir(const char* dirname, const char* filename) {
   struct dirent *dp;
   struct stat s;
   DIR *dir;
+  char* path;
 
   if(dir = opendir(dirname)) {
-
     fchdir(dirfd(dir));
     do
       if ((dp = readdir(dir))
           && (stat(dp->d_name, &s) != -1))
         if (S_ISREG (s.st_mode)
             && (strcasestr(dp->d_name, filename) != NULL))
-          puts(dp->d_name);
+          {
+	    path = realpath(dp->d_name, NULL);
+            puts(path);
+	    free(path);
+          }
         else if (S_ISDIR (s.st_mode)
                  && strcmp(dp->d_name, ".")
                  && strcmp(dp->d_name, ".."))
-          {
-            listdir(dp->d_name, filename);
-          }
+          listdir(dp->d_name, filename);
     while(dp);
     closedir(dir);
   }
